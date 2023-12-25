46c1efb07a6fb914aadf31144fe0d512d3a9b806
MeerMuneeb <jimmykuddon@gmail.com>
2023-12-26 00:37:29 +0500

    navigation set

--- screens/Properties.js
+++ screens/Properties.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <TextInput style={styles.search} placeholder="Search Properties" placeholderTextColor="#0008"/>
+      </View>
       <Text style={styles.text}>Home Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  search:{
+    backgroundColor: '#F0F5FF',
+    marginHorizontal: 20,
+    height: 35,
+    borderRadius: 35 / 2,
+  },
+  headerContainer:{
+    backgroundColor: '#fff',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#F0F5FF',
   },
   text: {
     fontSize: 20,
