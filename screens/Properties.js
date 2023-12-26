2ba79adf9a47ef7fc2b78f55ef3544013080d34c
MeerMuneeb <jimmykuddon@gmail.com>
2023-12-26 23:18:30 +0500

    working on gallery

--- screens/Properties.js
+++ screens/Properties.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Image, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-        <TextInput style={styles.search} placeholder="Search Properties" placeholderTextColor="#0008"/>
+        <View style={styles.search}>
+          <View style={styles.searchIcon}><Image source={require('../images/mg.png')}/></View>
+          <TextInput style={styles.searchField} placeholder="Search Properties" placeholderTextColor="#0008"/>
+        </View>
+        <View style={styles.btn}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Go to Gallery  </Text>
+            <Image source={require('../images/back.png')} style={styles.arrowIcon} />
+          </TouchableOpacity>
+        </View>
       </View>
       <Text style={styles.text}>Home Screen</Text>
     </View>
@@ -13,14 +22,29 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  btn:{
+    alignItems: 'flex-end',
+    marginEnd: 20,
+  },
   search:{
     backgroundColor: '#F0F5FF',
     marginHorizontal: 20,
     height: 35,
     borderRadius: 35 / 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10
+  },
+  searchField:{
+    backgroundColor: '#F0F5FF',
+    color: '#000'
+  },
+  searchIcon:{
+    padding: 5
   },
   headerContainer:{
     backgroundColor: '#fff',
+    elevation: 5
   },
   container: {
     flex: 1,
@@ -30,5 +54,29 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: '#333', 
+  },
+  button: {
+    backgroundColor: '#28246A', 
+    padding: 5,
+    borderRadius: 5,
+    marginVertical: 10,
+    flexDirection: 'row',
+    width: 115,
+    height: 25.5,
+    padding: 3,
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    borderRadius: 25
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 12.5,
+    fontWeight: 'normal',
+  },
+  arrowIcon: {
+    width: 20, 
+    height: 20, 
+    resizeMode: 'contain',
   },
 });
